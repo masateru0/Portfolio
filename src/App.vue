db9ef3897dec74a738d5610ad6256605367fcc3b
masateru0 <masman080@yahoo.co.jp>
2025-06-16 16:18:25 +0900

    起点をApp.vueからTopPage.vueに変更

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
-    <el-button class="btn btn-primary" type="primary">Hello Element Plus</el-button>
+    <div></div>
 </template>
 
 <script setup>
-import { ElButton } from 'element-plus'
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import TopPage from './TopPage.vue';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-createApp(App).use(ElementPlus).mount('#app')
+createApp(TopPage).use(ElementPlus).mount('#app')
