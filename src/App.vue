62fd3e837c3584b28da4ae9b272a5f796e32e615
masateru0 <masman080@yahoo.co.jp>
2025-06-16 15:04:29 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+    <el-button class="btn btn-primary" type="primary">Hello Element Plus</el-button>
+</template>
+
+<script setup>
+import { ElButton } from 'element-plus'
+</script>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+createApp(App).use(ElementPlus).mount('#app')
