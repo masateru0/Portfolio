4a496d60dcc15238b5b9f37d92d2d72eff0561ca
masateru0 <masman080@yahoo.co.jp>
2025-06-18 11:14:35 +0900

    FirstView完成

--- src/components/FirstView.vue
+++ src/components/FirstView.vue
@@ -1,5 +1,33 @@
 <template>
-    <div></div>
+    <div class="img-container">
+        <img
+            class="first-view-img"
+            src="@/assets/first-view.png"
+            alt="FirstView"
+        >
+        <p class="overlay-text">Building My Future Code.</p>
+    </div>
 </template>
 
 <script setup></script>
+
+<style scoped>
+
+.img-container {
+    position: relative;
+    display: inline-block;
+}
+
+.first-view-img {
+    width: 100%;
+    height: auto;
+}
+
+.overlay-text {
+    position: absolute;
+    bottom: 8%;
+    left: 13%;
+    color: #583C27;
+    font-size: 1.5rem;
+    font-weight: bold;}
+</style>
