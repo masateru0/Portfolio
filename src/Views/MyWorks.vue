b0aa233927e3b5c41adbda45649ccc6e575912d5
masateru0 <masman080@yahoo.co.jp>
2025-06-21 02:56:30 +0900

    MyWorks.vue実装。gridを画面中央に寄せる必要あり。

--- src/Views/MyWorks.vue
+++ src/Views/MyWorks.vue
@@ -1,5 +1,70 @@
 <template>
-    <div></div>
+    <section class="works-section">
+        <ContentsText title="Works" />
+
+        <div class="works-grid">
+            <WorkCard
+                v-for="(work, index) in works"
+                :key="index"
+                :title="work.title"
+                description="work.description"
+                :image="work.image"
+            />
+        </div>
+    </section>
 </template>
 
-<script setup></script>
+<script setup>
+import ContentsText from '@/components/ContentsText.vue'
+import WorkCard from '@/components/WorkCard.vue'
+
+const works = [
+    {
+        title: 'ポートフォリオサイト',
+        description: 'Desine / Coding',
+        image: new URL('@/assets/about-me-img.jpg', import.meta.url).href
+    },
+    {
+        title: 'ポートフォリオサイト',
+        description: 'Desine / Coding',
+        image: new URL('@/assets/works/portfolio1.png', import.meta.url).href
+    },
+    {
+        title: 'ポートフォリオサイト',
+        description: 'Desine / Coding',
+        image: new URL('@/assets/works/portfolio1.png', import.meta.url).href
+    },
+    {
+        title: 'ポートフォリオサイト',
+        description: 'Desine / Coding',
+        image: new URL('@/assets/about-me-img.jpg', import.meta.url).href
+    },
+    {
+        title: 'ポートフォリオサイト',
+        description: 'Desine / Coding',
+        image: new URL('@/assets/works/portfolio1.png', import.meta.url).href
+    },
+    {
+        title: 'ポートフォリオサイト',
+        description: 'Desine / Coding',
+        image: new URL('@/assets/works/portfolio1.png', import.meta.url).href
+    },
+]
+</script>
+
+<style scoped>
+.works-section {
+    background-color: #D6F2F8;
+    padding: 4rem 2rem;
+}
+
+.works-grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    gap: 2rem;
+    max-width: 1200px;
+    margin-inline: auto;
+    margin-bottom: 2rem;
+    margin-top: 3rem;
+}
+</style>
---
+++ src/components/WorkCard.vue
@@ -0,0 +1,64 @@
+<template>
+    <el-card class="work-card" shadow="hover" style="max-width: 300px">
+        <template #header>
+            <div class="card-header">
+                <h3 class="work-title">{{ title }}</h3>
+                <p class="work-description">{{ description }}</p>
+            </div>
+        </template>
+
+        <img :src="image" alt="Work Image" class="work-img" />
+    </el-card>
+</template>
+
+<script setup>
+defineProps({
+    title: String,
+    description: String,
+    image: String,
+})
+</script>
+
+<style scoped>
+.work-card {
+    border-radius: 5px;
+    overflow: hidden;
+    padding: 0;
+    transition: transform 0.2s;
+    width: 1200px;
+    height: auto;
+}
+
+.work-card:hover {
+    transform: translateY(-5px);
+}
+
+.card-header {
+    max-width: 220px;
+    max-height: 40px;
+    text-align: center;
+    margin: 0 auto;
+}
+
+.work-img {
+    width: 100%;
+    height: auto;
+    border-radius: 4px 4px 0 0;
+}
+
+.work-info {
+    padding: 1rem;
+    text-align: center;
+}
+
+.work-title {
+    font-size: 16px;
+    font-weight: bold;
+    color: #333;
+}
+
+.work-description {
+    font-size: 14px;
+    color: #666;
+}
+</style>
