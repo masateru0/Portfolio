ff1b06f6d1c8959ee1d76d0b03ec68d2ed87f77a
masateru0 <masman080@yahoo.co.jp>
2025-06-30 02:06:59 +0900

    worksのイメージホバー時にアニメーションがつくよう修正

--- src/components/WorkCard.vue
+++ src/components/WorkCard.vue
@@ -29,6 +29,7 @@ defineProps({
     align-items: center;
     cursor: pointer;
 }
+
 .work-img {
     width: 100%;
     height: 250px;
@@ -36,17 +37,29 @@ defineProps({
     border-radius: 8px;
     margin-bottom: 16px;
     display: block;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.12);
+    transition:
+        transform 0.3s ease,
+        box-shadow 0.3s ease;
 }
+
+.work-img:hover {
+    transform: scale(1.05);
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.18);
+}
+
 .work-info {
     padding-left: 4px;
     width: 100%;
 }
+
 .work-title {
     font-weight: bold;
     font-size: 1.1rem;
     color: #4b3c2a;
     margin-bottom: 4px;
 }
+
 .work-description {
     color: #444;
     font-size: 1rem;
